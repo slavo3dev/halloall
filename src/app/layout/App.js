0ea2b8812ce165785f2165533130168db660dca9
slavo7 <slavo@mimicom24.com>
2018-06-20 21:28:17 -0400

    adding NavBar to the page

--- src/app/layout/App.js
+++ src/app/layout/App.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar/NavBar'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <NavBar/>
         <EventDashboard/>
       </div>
     );
