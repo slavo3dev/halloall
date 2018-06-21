8c2a6001927b627d60850f295f108704b2d6e0a2
slavo7 <slavo@mimicom24.com>
2018-06-20 21:17:07 -0400

    Right Column

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -8,6 +8,9 @@ class EventDashboard extends Component {
                 <Grid.Column width={10}>
                     <h2>Left Column</h2>
                 </Grid.Column>
+                <Grid.Column width={6}>
+                    <h2>Right Column</h2>
+                </Grid.Column>
             </Grid>
         )
     }
