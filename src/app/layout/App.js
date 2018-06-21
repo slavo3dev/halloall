d74a7f27e8a36541e97ea50d85d274ade203d53b
slavo7 <slavo@mimicom24.com>
2018-06-20 21:18:43 -0400

    adding component

--- src/app/layout/App.js
+++ src/app/layout/App.js
@@ -1,15 +1,12 @@
 import React, {Component} from 'react';
 import {Button} from 'semantic-ui-react';
+import EventDashboard from '../../features/event/EventDashboard/EventDashboard'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>Hello Slavo</p>
-        <p>First App</p>
-        <button className='ui icon button'>
-          <i className="smile icon"></i>CSS Button</button>
-        <Button icon='smile' content='React Button'/>
+        <EventDashboard/>
       </div>
     );
   }
