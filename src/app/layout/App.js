b3d210ee44a373c237203ce5f620b4e5d9f39ee5
slavo7 <slavo@mimicom24.com>
2018-06-20 21:39:10 -0400

    adding container element

--- src/app/layout/App.js
+++ src/app/layout/App.js
@@ -1,14 +1,15 @@
 import React, {Component} from 'react';
+import {Container} from 'semantic-ui-react'
 import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
-import NavBar from '../../features/nav/NavBar/NavBar'
+import NavBar from '../../features/nav/NavBar/NavBar';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <Container className="main">
         <NavBar/>
         <EventDashboard/>
-      </div>
+      </Container>
     );
   }
 }
