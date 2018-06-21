9f7f220db0c5ba2d5ba323cdcbaa18de6e51addf
slavo7 <slavo@mimicom24.com>
2018-06-20 21:14:31 -0400

    importing Grid

---
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {Grid} from 'semantic-ui-react';
+
+class EventDashboard extends Component {
+    render() {
+        return (
+            <Grid></Grid>
+        )
+    }
+}
+
+export default EventDashboard;
