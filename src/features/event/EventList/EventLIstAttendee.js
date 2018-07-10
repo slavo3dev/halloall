508431ae98ac11a9982766a568f05cda8dabdc73
slavo7 <slavo@mimicom24.com>
2018-07-09 21:48:38 -0400

    adding 3 new componets

---
+++ src/features/event/EventList/EventLIstAttendee.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class EventListAttendee extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Event List Atendee</h1>
+            </div>
+        )
+    }
+};
+
+export default EventListAttendee;
---
+++ src/features/event/EventList/EventLIstItems.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class EventListItems extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Event List Items</h1>
+            </div>
+        )
+    }
+};
+export default EventListItems;
